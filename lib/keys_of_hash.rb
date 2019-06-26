9b4f1dc50f62414230c954e5a8653c2a5676cbb8
Ursula Miller <ursula.miller@gmail.com>
2019-06-26 04:29:40 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+<<<<<<< HEAD
 
 
 class Hash
@@ -29,4 +30,26 @@ end
 #  "kangaroo"=> "Australia",
 #  "tomato frog"=>"Madagascar",
 #  "koala"=>"Australia"} 
+=======
+class Hash
+  def keys_of(*arguments)
+    set_of_args = []
+#    arguments.each do |things|
+#      set_of_args << things
+ #     binding.pry
+#     end
+#     set_of_args.each
+    set_of_args
+  end
+end
+
+
+animals = 
+  {"sugar glider"=>"Australia",
+  "aye-aye"=> "Madagascar",
+  "red-footed tortoise"=>"Panama",
+  "kangaroo"=> "Australia",
+  "tomato frog"=>"Madagascar",
+  "koala"=>"Australia"} 
+>>>>>>> 83c127debe5c929f27511c1db020f3abe5f2a76e
 
